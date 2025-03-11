50ddd448788d02f2a3be2982d26e1ee308c0f32b
pedrohenrik11 <pedroaguiarjesus@gmail.com>
2025-03-11 09:39:55 -0300

    feat: projeto Devops

---
+++ ApiPiaget/Models/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiPiaget.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string  RGM  { get; set; }
+        public decimal Notas { get; set; }
+
+
+
+    }
+}
